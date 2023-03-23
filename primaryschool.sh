65f43392e09b002330d0faa4fc8f149c09292446
larryw3i <larryw3i@163.com>
2023-03-23 20:11:44 +0800

    backup.

--- primaryschool.sh
+++ primaryschool.sh
@@ -1,17 +1,57 @@
 
-venv_dir="${PWD}/venv"
-py_version="$(python --version)"
+app_name="primaryschool"
+app_name_sh="${app_name}.sh"
+app_name_py="${app_name}.py"
+venv_dir_name="venv"
+venv_dir_path="${PWD}/${venv_dir_name}"
+py_version="$(python3 --version)"
 global_parameters=$@
 
-use_venv(){
-    if ! [[ -d ${venv_dir} ]];
-    then
-        if [[ ${py_version}==*"3.11"* ]];
-        then
-            python3 venv ${venv_dir}
-        fi
-
-        
-    fi
+echo_use_venv_y(){
+    echo "Virtual environment is used."
 }
 
+use_venv(){
+    if ! [[ -d "${venv_dir_path}" ]];
+    then
+        if [[ ${py_version} == *"3.11"* ]];
+        then
+            python3 -m venv ${venv_dir_path}
+        elif [[ -f $(which virtualenv) ]];
+        then
+            virtualenv venv
+        else
+            echo "`virtualenv` is not installed!"
+        fi
+    fi
+    
+    if ! [[ -d "${venv_dir_path}" ]];
+    then
+        return 0
+    fi
+
+    if [[ "$SHELL" == *"bash"* ]];
+    then
+        . ${venv_dir_path}/bin/activate
+        echo_use_venv_y
+    else
+        . ${venv_dir_path}/bin/activate.fish
+        echo_use_venv_y
+    fi
+    return 1        
+}
+
+if [[ $# == 0 ]];
+then
+    echo "${app_name_sh} is used."
+elif [[ $1 == "use_venv" ]];
+then
+    use_venv
+else
+    use_venv
+    if ! [[ $? == 1 ]];
+    then 
+        return
+    fi
+    $@
+fi
