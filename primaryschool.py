a90444313dd89f4faad564df397d3bb8b0501937
larryw3i <larryw3i@163.com>
2023-03-26 03:20:38 +0800

    Backup.

--- primaryschool.py
+++ primaryschool.py
@@ -1,23 +1,52 @@
-from pathlib import *
 
+import getopt
+from pathlib import *
+from primaryschool.settings import *
 
 def get_dev_requirements_for_pypi():
-    requirements = [("")]
+    
+    requirements = [
+        (
+            "black",
+            None,
+            "https://github.com/psf/black",
+            "MIT license",
+            "https://github.com/psf/black/blob/main/LICENSE"
+        ),
+        (
+            "pre-commit",
+            None,
+            "https://github.com/pre-commit/pre-commit",
+            "MIT license",
+            "https://github.com/pre-commit/pre-commit/blob/main/LICENSE"
+        )
+    ]
+    requirements += get_requirements_for_pypi()
     return requirements
     pass
 
 
-def get_dev_requirements_for_apt():
-    requirements = [("")]
+
+def get_latest_dev_requirements_for_pypi():
+    requirements = get_dev_requirements_for_pypi()
+    for r in requirements:
+        r[1] = None
+    requirements += get_latest_requirements_for_pypi()
     return requirements
     pass
 
+def get_opt_shortopts_with_comment():
+    shortopts = [
+        ("t", _("`t` for testing."))
+    ]
 
-def get_dev_requirements_for_dnf():
-    requirements = [("")]
-    return requirements
-    pass
-
+def get_opt_shortopts():
+    shortopts = [
+        "t" + # `t` for testing.
+        "s" + # `s` for 
+    ]
+    return  shortopts 
 
 if __name__ == "__main__":
+    print("Have fun!")
     pass
--- primaryschool/settings.py
+++ primaryschool/settings.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import *
 
 app_name = "primaryschool"
-app_version = "0.0.23"
+app_version = "0.0.24"
 app_author = "larryw3i"
 app_author_email = "larryw3i@163.com"
 app_maintainer = app_author
@@ -18,6 +18,8 @@ app_contributors = [
 app_sponsors = [""]
 
 install_prefix = "python -m pip install "
+
+# deprecated
 requirements = [
     # product
     [  # ('requirement_name','version','project_url','License','license_url')
@@ -54,22 +56,56 @@ requirements = [
 
 
 def get_requirements_for_apt():
-    """For python 3.11, the Python environment is externally managed, so python
-    packages should be installed via `apt`.
+    """For python 3.11, the Python environment is externally managed by default
+    , so python packages should be installed via `apt`.
     The Format of requirements:
     ```python3
     [
-        ("package_name","package_version(N/A)","license","license_url"),
+        (
+            "package_name",
+            "package_version(N/A)",
+            "package_url",
+            "license",
+            "license_url"),
     ]
     ```
     """
-    requirements = [("")]
+    requirements = [
+        (
+            "python3-pygame",
+            None,
+            "https://www.pygame.org/" "GNU LGPL version 2.1",
+            "https://github.com/pygame/pygame/blob/main/docs/LGPL.txt",
+        ),
+        (
+            "python3-appdirs",
+            None,
+            "https://github.com/ActiveState/appdirs",
+            "MIT",
+            "https://github.com/ActiveState/appdirs/blob/master/LICENSE.txt",
+        ),
+    ]
     return requirements
     pass
 
 
 def get_requirements_for_pypi():
-    requirements = [("")]
+    requirements = [
+        (
+            "pygame",
+            None,
+            "https://github.com/pygame/pygame",
+            "LGPL v2",
+            "https://github.com/pygame/pygame/blob/main/docs/LGPL.txt",
+        ),
+        (
+            "appdirs",
+            None,
+            "https://github.com/ActiveState/appdirs",
+            "MIT",
+            "https://github.com/ActiveState/appdirs/blob/master/LICENSE.txt",
+        ),
+    ]
     return requirements
     pass
 
@@ -100,3 +136,12 @@ def get_requirements_product():
 def install_requirements_product():
     requirements_product = get_requirements_product()
     os.system(install_prefix + requirements_product)
+    pass
+
+
+def get_latest_requirements_for_pypi():
+    requirements = get_requirements_for_pypi()
+    for r in requirements:
+        r[1] = None
+    return requirements
+    pass
