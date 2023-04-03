346985bfaa71e4d1d070cfe1abc9ec947e2c19b3
larryw3i <larryw3i@163.com>
2023-04-03 18:26:39 +0800

    Backup.

--- primaryschool.py
+++ primaryschool.py
@@ -1,44 +1,51 @@
+import argparse
 import getopt
-from pathlib import *
+import sys
 
-from primaryschool.pssettings import *
+import primaryschool
+from primaryschool import *
+from primaryschool.psdep import *
+from primaryschool.psl10n import _
 
 
-def get_dev_requirements_for_pypi():
-    requirements = [
-        (
-            "black",
-            None,
-            "https://github.com/psf/black",
-            "MIT license",
-            "https://github.com/psf/black/blob/main/LICENSE",
-        ),
-        (
-            "pre-commit",
-            None,
-            "https://github.com/pre-commit/pre-commit",
-            "MIT license",
-            "https://github.com/pre-commit/pre-commit/blob/main/LICENSE",
-        ),
-    ]
-    requirements += get_requirements_for_pypi()
-    return requirements
+def test():
+    print_nl(
+        "req_names_pypi",
+        req_names_pypi,
+        "req_names_with_version_pypi",
+        req_names_with_version_pypi,
+        "setup_reqs",
+        setup_reqs,
+    )
+
+def update_pyproject_toml():
     pass
 
+parser = argparse.ArgumentParser(
+    prog=_("primaryschool.py"),
+    add_help=True,
+    description=_("Some unitls for primaryschool."),
+)
 
-def get_latest_dev_requirements_for_pypi():
-    requirements = get_dev_requirements_for_pypi()
-    for r in requirements:
-        r[1] = None
-    requirements += get_latest_requirements_for_pypi()
-    return requirements
+
+parser.add_argument(
+    "-t",
+    "--test",
+    action="store_true",
+    help=_("Run the testings."),
+)
+
+
+argv = sys.argv[1:]
+if len(argv) < 1:
     pass
 
+args = parser.parse_args()
+run_test = args.test
 
-def get_opt_shortopts_with_comment():
-    shortopts = [("t", _("`t` for testing."))]
 
+if run_test:
+    test()
+    exit()
 
-if __name__ == "__main__":
-    print("Have fun!")
-    pass
+pass
