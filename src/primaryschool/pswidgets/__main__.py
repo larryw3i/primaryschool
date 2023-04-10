0f2967639c5f3b9481309c36eb31de8ca377156c
larryw3i <larryw3i@163.com>
2023-04-10 10:37:50 +0800

    Update 'primaryschool.pswidgets' test.

--- src/primaryschool/pswidgets/__main__.py
+++ src/primaryschool/pswidgets/__main__.py
@@ -8,13 +8,32 @@ def test():
     top_widget = TopWidget(mainloop=False)
     root_widget = top_widget.rootw
     mainframe = top_widget.mainframe
+
+    buttons_l = []
     for i in range(20):
-        ttk.Button(mainframe, text=str(i) * 5).pack(side=LEFT)
+        buttons_l.append(ttk.Button(mainframe, text=str(i) * 5))
+        pass
+    
+    buttons_t = []
+    for i in range(20):
+        buttons_t.append(ttk.Button(mainframe, text=str(i) * 5))
         pass
 
-    for i in range(20):
-        ttk.Button(mainframe, text=str(i) * 5).pack(side=TOP)
-        pass
+    for bl in buttons_l:
+        bl.pack(side="left")
+
+    for bt in buttons_t:
+        bt.pack(side="top")
+
+    for bt in buttons_t:
+        bt.forget()
+        bt.destroy()
+
+
+    for bl in buttons_l:
+        bl.forget()
+        bl.destroy()
+
 
     top_widget.mainloop()
     print_nl()
--- test0.sh
+++ test0.sh
@@ -1,28 +1,4 @@
 pypitxt_path="primaryschool/psdep/pypi.txt"
 
-dindex=-1
-pypideps=""
-breaked_line=""
-while IFS= read -r line; do
-    echo $line
-    if [[ $line == *'\'* ]];
-    then
-        breaked_line+="${line::-1}"
-        continue
-    fi
-
-    if  [[ ${breaked_line} != "" ]];
-    then
-        line="${breaked_line}${line}"
-        breaked_line=""
-    fi
-
-    if [[ $((dindex%6)) == 0 ]];
-    then
-        pypideps+=" $line"
-    fi
-    dindex=$((dindex+1))
-done < $pypitxt_path
-dindex=-1
-echo $pypideps
+read -a requirements < $pypitxt_path
 
