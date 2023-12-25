62e1cdf20ae021ed73b1e8d3ef061146106cf491
larry <larryw3i@163.com>
2023-12-25 22:39:03 +0800

    Update l10n directory.

--- src/primaryschool/psl10n/__init__.py
+++ src/primaryschool/psl10n/__init__.py
@@ -8,27 +8,34 @@ from pathlib import Path
 import primaryschool
 
 sys_lang_code = locale.getdefaultlocale()[0]
-locale_path = locale_dir_path = os.path.abspath(os.path.dirname(__file__))
-project_path = str(Path(__file__).parent.parent.parent.absolute())
-po0_path = os.path.join(
-    locale_dir_path, "en_US", "LC_MESSAGES", "primaryschool.po"
-)
 
-mo0_path = os.path.join(
-    locale_dir_path, "en_US", "LC_MESSAGES", "primaryschool.mo"
-)
+locale_path = locale_dir_path = Path(os.path.abspath(os.path.dirname(__file__)))
+project_path = Path(__file__).parent.parent.parent.absolute()
+lc_messages_path = Path(locale_dir_path) / "en_US" / "LC_MESSAGES"
+
+po0_path = lc_messages_path / "primaryschool.po"
+
+mo0_path = lc_messages_path / "primaryschool.mo"
 
 if not os.path.exists(mo0_path):
     print(
         f"It seems you run 'primaryschool' via development mode. "
-        + f"'{mo0_path}' doesn't exist. Try to compile it."
+        + f"'{mo0_path}' doesn't exist. Try to compile it..."
     )
-    subprocess.Popen(
-        f"cd {project_path};"
-        + f". {project_path}/primaryschoo1.sh msg_fmt;"
-        + f'echo "Compile successfully!"',
-        shell=True,
-    )
+    for root, dirs, files in os.walk(lc_messages_path.as_posix()):
+        for f in files:
+            if f.endswith(".po"):
+                po_file_path = Path(root, f)
+                mo_file_path = po_file_path.with_suffix(".mo")
+                os.system(f"msgfmt -vv -o {mo_file_path} {po_file_path}")
+        pass
+
+    # subprocess.Popen(
+    #    f"cd {project_path};"
+    #    + f". {project_path}/primaryschoo1.sh msg_fmt;"
+    #    + f'echo "Compile successfully!"',
+    #    shell=True,
+    # )
 
 locale_langcodes = [
     d
